ab11add1990c326a033be37c2d590659ab99b2f5
Angel <angeldavidq26@gmail.com>
2026-09-04 07:03:55 -0500

    feat(perfil): agregar perfil profesional de Angel Quintana

--- js/aprendices_db.js
+++ js/aprendices_db.js
@@ -32,8 +32,39 @@ const APRENDICES_DATA = [
     },
     "fraseFavorita": "La práctica constante y la disciplina convierten el conocimiento en maestría.",
     "estado": "Guiando la sesión de aprendizaje"
+  },
+  {
+    "id": "angel-quintana",
+    "nombreCompleto": "Angel Quintana",
+    "ficha": "3294152",
+    "programa": "Análisis y Desarrollo de Software",
+    "rol": "Aprendiz / Desarrollador Junior",
+    "biografia": "Aprendiz SENA apasionado por el desarrollo web y el aprendizaje de nuevas tecnologías de programación.",
+    "habilidades": [
+      "Git",
+      "GitHub",
+      "HTML5",
+      "CSS3",
+      "JavaScript"
+    ],
+    "intereses": [
+      "Desarrollo Web",
+      "Programación"
+    ],
+    "avatar": "assets/avatares/avatar_carlos.svg",
+    "redes": {
+      "github": "https://github.com/angeldavidq26",
+      "linkedin": "https://linkedin.com/in/angeldavidq26",
+      "portafolio": "https://angeldavidq26.github.io/mi-perfil-sena"
+    },
+    "fraseFavorita": "La práctica constante hace al maestro del código y mateo es un peo",
+    "estado": "Listo para colaborar"
   }
+
+
  ];
+
+ 
 
 // Si se ejecuta en entorno Node/módulos
 if (typeof module !== 'undefined' && module.exports) {
