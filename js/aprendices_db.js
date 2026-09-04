57f7f71adc911088043b4f02cea1c2c84878bc9d
kerlon_velasquez <kerlonvelasquez525@gmail.com>
2026-09-04 07:00:16 -0500

    feat(perfil): agregar perfil profesional de kerlon velasquez

--- js/aprendices_db.js
+++ js/aprendices_db.js
@@ -32,7 +32,34 @@ const APRENDICES_DATA = [
     },
     "fraseFavorita": "La práctica constante y la disciplina convierten el conocimiento en maestría.",
     "estado": "Guiando la sesión de aprendizaje"
-  }
+  },
+  {
+  "id": "kerlon-velasquez",
+  "nombreCompleto": "Kerlon Velasquez",
+  "ficha": "3294152",
+  "programa": "Análisis y Desarrollo de Software",
+  "rol": "Aprendiz / Desarrollador Junior",
+  "biografia": "Aprendiz SENA apasionado por el desarrollo web y el aprendizaje de nuevas tecnologías de programación en constante aprendizaje.",
+  "habilidades": [
+    "Git",
+    "GitHub",
+    "HTML5",
+    "CSS3",
+    "JavaScript"
+  ],
+  "intereses": [
+    "Desarrollo Web",
+    "Programación"
+  ],
+  "avatar": "assets/avatares/avatar_carlos.svg",
+  "redes": {
+    "github": "https://github.com/kerlonvelasquez525-star",
+    "linkedin": "https://www.linkedin.com/in/kerlon-velasquez-72180b429/",
+    "portafolio": "https://github.com/kerlonvelasquez525-star/crear_componentes_frontend"
+  },
+  "fraseFavorita": "La práctica constante hace al maestro del código.",
+  "estado": "Listo para colaborar"
+}
  ];
 
 // Si se ejecuta en entorno Node/módulos
